6778da02ef56a2b793e0ddc8cec219bb4a2f62c1
KamoliddinSaparaliyev <saparaliyevkamoliddin131@gmail.com>
2024-05-10 19:16:20 +0500

    add: http joi validator

---
+++ src/shared/htto-validator.ts
@@ -0,0 +1,67 @@
+import { Request } from "express";
+import Joi, { ValidationResult, ObjectSchema } from "joi";
+import HttpException from "../models/http-exeption.model";
+
+/**
+ * Data structure representing request data.
+ */
+interface RequestData {
+  body?: Request["body"];
+  params?: Request["params"];
+  query?: Request["query"];
+}
+
+/**
+ * Data structure representing validation schema.
+ */
+interface ValidationSchema {
+  body?: ObjectSchema;
+  params?: ObjectSchema;
+  query?: ObjectSchema;
+}
+
+/**
+ * Validates the request data against the provided schema.
+ * @param {RequestData} schemaData - Request data to be validated.
+ * @param {ValidationSchema} validationSchema - Validation schema to validate against.
+ */
+const httpValidator = (
+  schemaData: RequestData,
+  validationSchema: ValidationSchema
+): void => {
+  /**
+   * Validates the data against the provided schema.
+   * @param {any} data - Data to be validated.
+   * @param {ObjectSchema | undefined} schema - Joi schema to validate against.
+   * @returns {ValidationResult} - Validation result.
+   */
+  const validate = (data: any, schema?: ObjectSchema): ValidationResult => {
+    const result = schema?.validate(data, { abortEarly: false });
+    return (
+      result || { error: new Joi.ValidationError("", [], data), value: data }
+    );
+  };
+
+  const errors: string[] = [];
+
+  Object.keys(schemaData).forEach((key) => {
+    const schema = schemaData[key as keyof RequestData];
+    const validSchema = validationSchema[key as keyof ValidationSchema];
+
+    if (schema && validSchema) {
+      const { error } = validate(schema, validSchema);
+      if (error) {
+        errors.push(...error.details.map((d) => d.message));
+      }
+    }
+  });
+
+  if (errors.length > 0) {
+    throw new HttpException(400, {
+      success: false,
+      message: errors.join(", "),
+    });
+  }
+};
+
+export default httpValidator;
